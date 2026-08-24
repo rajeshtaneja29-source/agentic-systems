2f6e3ad2290431a43ae68897bf17e88191c85ec4
rajeshtaneja29-source <rajeshtaneja29@gmail.com>
2026-08-24 23:34:35 +0530

    Add files via upload

---
+++ 01-Programming-Foundations/01-Intro-to-Programming-and-Python-Basics/code/comparisonOperations.py
@@ -0,0 +1,20 @@
+age = 18
+print(age == 18)  # True 
+print(age == 20)  # False
+
+score = 85
+print(score > 105)   # False
+print(score >= 85)  # True
+
+# Store results in variables
+is_adult = age >= 18
+print(is_adult)     # True
+print(type(is_adult))  # <class 'bool'>
+
+print(5 == 5.1) # False
+
+print(5 == 5.0) # True
+
+print(5 == 5.00000000000000000000000001) # True
+
+print(5 == 5.0000000001) # False
